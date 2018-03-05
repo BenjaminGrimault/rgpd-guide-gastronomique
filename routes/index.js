27da0affb9ef1a73f76a6069928adf12b78455ec
Aethyn <mmi.theleme@gmail.com>
2018-03-05 15:21:41 +0100

    Authentification v1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Accueil
+router.get('/', function(req, res, next){
+    if(!req.session.user){
+        res.redirect('/login');
+    } else {
+        res.render('index', {
+            username: req.session.user.username
+        });
+    }
+});
+
+// Accès à la page de connexion
+router.get('/login', function(req, res, next){
+    if(!req.session.user){
+        res.render('login');
+    } else {
+        res.redirect('/');
+    }
+    
+});
+
+// Envoi du formulaire de connexion
+router.post('/login', function(req, res, next){
+    // @WARNING : bypass de la logique backend de connexion pour le moment
+    req.session.user = {
+        username: req.body.username
+    }
+    res.redirect('/');
+});
+
+// Provoque la déconnexion
+router.post('/logout', function(req, res, next){
+    req.session.destroy();
+    res.redirect('/login');
 });
 
 module.exports = router;
