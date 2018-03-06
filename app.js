4b19b3e0c07be2a5f1d76014a619fe9d090a8e14
Aethyn <mmi.theleme@gmail.com>
2018-03-06 10:07:38 +0100

    Modification de l'accès à la DB

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+const express = require('express');
+const app = express();
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-require('./services/db');
-
-db.getUsers(function(users) {
-    console.log(users);
-});
-
-var app = express();
-
-// view engine setup
+// Moteur de template des vues
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
+// A décommenter après avoir placé la favicon dans /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -34,25 +26,38 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.use('/', index);
-app.use('/users', users);
+const url = 'mongodb://localhost:27017';
+const dbName = 'rgpd-guide-gastronomique';
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-// error handler
-app.use(function(err, req, res, next) {
-// set locals, only providing error in development
-res.locals.message = err.message;
-res.locals.error = req.app.get('env') === 'development' ? err : {};
+    const db = client.db(dbName);
 
-// render the error page
-res.status(err.status || 500);
-res.render('error');
+    const index = require('./routes/index')(db);
+    const users = require('./routes/users')(db);
+
+    app.use('/', index);
+    app.use('/users', users);
+
+    // Capture les 404 et les passe au gestionnaire d'erreur
+    app.use(function(req, res, next) {
+        let err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // Gestionnaire d'erreur
+    app.use(function(err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
 });
 
 module.exports = app;
