194f7d4b9fdd7b11c8f4f5f5ae5d3c298d3c6a9e
Benjamin Grimault <benjamin.grimault@gmail.com>
2018-03-06 15:30:04 +0100

    Create user on register

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('../models/user');
 const router = express.Router();
 const db = mongoose.connection;
 
@@ -8,7 +9,20 @@ router.get('/create', function(req, res, next) {
 });
 
 router.post('/create', function(req, res, next) {
-    // @WARNING : Ajouter ici l'insertion d'un nouvel user
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+    });
+
+    user.save(function (err, user) {
+        if (err) {
+            return console.error(err);
+        } else {
+            return console.log('User créé');
+        }
+    });
+
     // Là je bypass en créant l'user et en le ramenant à l'accueil
     req.session.user = {
         username: req.body.username
